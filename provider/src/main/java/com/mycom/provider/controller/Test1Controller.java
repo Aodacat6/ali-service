1b2bff1388ba05b03cbbc2cfc4b4c16f6b7a7837
songdalin <songdalin@qdcares.cn>
2022-04-20 15:39:21 +0800

    增加了gateway

---
+++ provider/src/main/java/com/mycom/provider/controller/Test1Controller.java
@@ -0,0 +1,32 @@
+package com.mycom.provider.controller;
+
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：songdalin
+ * @date ：2022-4-20 上午 10:26
+ * @description：
+ * @modified By：
+ * @version: 1.0
+ */
+@RestController
+@RequestMapping("/provider1")
+public class Test1Controller implements EnvironmentAware {
+
+    private String type;
+
+
+    @GetMapping("/test")
+    public String test() {
+        return "hello, this is provier，" + type;
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.type = environment.getProperty("type");
+    }
+}
